47f016d0e5e01f365c109b4087183191960b297f
Miguel Portillo <mipor.278@gmail.com>
2022-12-03 09:32:12 -0500

    Solucionado errores

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,16 +4,17 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+// use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Traits\Uuids;
 
 class User extends Model
 {
-    use HasFactory, Notifiable, HasUuids;
+    use HasFactory, Notifiable, Uuids;
 
     /**
      * The attributes that are mass assignable.
