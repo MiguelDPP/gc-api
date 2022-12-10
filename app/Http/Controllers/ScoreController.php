96ae9935a818255387ab58993a05e854ec20cb24
SebastyanPi <113456613+SebastyanPi@users.noreply.github.com>
2022-12-09 22:34:30 -0500

    subiendos

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -211,7 +211,7 @@ class ScoreController extends Controller
         $longitud = count($arreglo);
         for ($i = 0; $i < $longitud; $i++) {
             for ($j = 0; $j < $longitud - 1; $j++) {
-                if ($arreglo[$j]['points'] > $arreglo[$j + 1]['points']) {
+                if ($arreglo[$j]['points'] < $arreglo[$j + 1]['points']) {
                     $temporal = $arreglo[$j];
                     $arreglo[$j] = $arreglo[$j + 1];
                     $arreglo[$j + 1] = $temporal;
